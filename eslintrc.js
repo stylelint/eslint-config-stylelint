3191acd754e66f6551a9bbb2f49f5c2a25ba6352
Aleks Hudochenkov <aleks@hudochenkov.com>
2018-10-19 20:46:42 +0300

    Add `padding-line-between-statements` rule (#55)

---
+++ eslintrc.js
@@ -0,0 +1,75 @@
+"use strict";
+
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:node/recommended",
+    "plugin:jest/recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "plugins": [
+    "node",
+    "sort-requires"
+  ],
+  "rules": {
+    "eqeqeq": "error",
+    "no-use-before-define": ["error", "nofunc"],
+    "no-var": "error",
+    "object-shorthand": "error",
+    "one-var": ["error", "never"],
+    "padding-line-between-statements": [
+      "error",
+      // Require blank lines after all directive prologues (e. g. 'use strict')
+      {
+        "blankLine": "always",
+        "prev": "directive",
+        "next": "*"
+      },
+      // Disallow blank lines between all directive prologues (e. g. 'use strict')
+      {
+        "blankLine": "never",
+        "prev": "directive",
+        "next": "directive"
+      },
+      // Require blank lines after every sequence of variable declarations
+      {
+        "blankLine": "always",
+        "prev": ["const", "let", "var"],
+        "next": "*"
+      },
+      // Blank lines could be between variable declarations
+      {
+        "blankLine": "any",
+        "prev": ["const", "let", "var"],
+        "next": ["const", "let", "var"]
+      },
+      // Require blank lines before all return statements
+      {
+        "blankLine": "always",
+        "prev": "*",
+        "next": "return"
+      },
+      // Require blank lines before and after all following statements
+      {
+        "blankLine": "always",
+        "prev": "*",
+        "next": ["for", "function", "if", "switch", "try"]
+      },
+      {
+        "blankLine": "always",
+        "prev": ["for", "function", "if", "switch", "try"],
+        "next": "*"
+      }
+    ],
+    "prefer-const": "error",
+    "sort-requires/sort-requires": "error",
+    "strict": ["error", "global"],
+    "jest/prefer-to-have-length": "error"
+  }
+};
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 "use strict"
 
-module.exports = require("./eslintrc.json")
+module.exports = require("./eslintrc.js")
