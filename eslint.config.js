1f1a4f3c947ea74732dcd33b54ff1112bcf3994d
jeddy3 <jeddy3@users.noreply.github.com>
2024-10-27 12:13:36 +0000

    Revert adding jest to local config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 'use strict';
 
 const config = require('./index.js');
-const jestConfig = require('./jest.js');
 
 module.exports = [
 	...config,
-	...jestConfig,
 	{
 		// TODO: Keep backward compatibility with CommonJS. We may delete after ESM migration.
 		languageOptions: {
