1df8ed8b29c4aaecbefd6506d5795f2d3c901801
Richard Hallows <jeddy3@users.noreply.github.com>
2024-10-27 12:09:50 +0000

    Update eslint.config.js
    Co-authored-by: Masafumi Koba <473530+ybiquitous@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const config = require('./index.js');
+const jestConfig = require('./jest.js');
 
 module.exports = [
 	...config,
+	...jestConfig,
 	{
 		// TODO: Keep backward compatibility with CommonJS. We may delete after ESM migration.
 		languageOptions: {
